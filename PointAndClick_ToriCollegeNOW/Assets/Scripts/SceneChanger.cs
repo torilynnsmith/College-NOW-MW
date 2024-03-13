35e405a9ee9865e334d82f9793662bc263fcb477
Tori Smith <torilynnsmith@gmail.com>
2024-03-13 12:28:15 -0400

    updated comments

--- PointAndClick_ToriCollegeNOW/Assets/Scripts/SceneChanger.cs
+++ PointAndClick_ToriCollegeNOW/Assets/Scripts/SceneChanger.cs
@@ -8,44 +8,56 @@ public class SceneChanger : MonoBehaviour
     //COLLEGE NOW IN CLASS SCRIPT
 
     //GLOBAL VARIABLES
-    public int sceneNumber;
-        //0 = StartScene
-        //1 = MainScene
-        //2 = EndScene
+    public int sceneNumber; //essentially does the same thing as sceneID in the Build Settings
+                            //0 = StartScene
+                            //1 = MainScene
+                            //2 = EndScene
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (sceneNumber == 0) //if we're in the Start Scene
+        if (sceneNumber == 0) //we're in the start scene
         {
-            //use Start Scene Controls
-            //Debug.Log("start scene controls");
-            StartSceneControls(); //load main scene
+            StartSceneControls(); //call StartSceneControls()
         }
-        else if (sceneNumber == 1) //if we're in the Main Scene
+        else if (sceneNumber == 1) //we're in the main scene
         {
-            //use Main Scene Controls
-            //Debug.Log("main scene controls");
+            MainSceneControls(); //call MainSceneControls()
         }
-        else if (sceneNumber == 2) //if we're in the End Scne
+        else if (sceneNumber == 2) //we're in the end scene
         {
-            //use End Scene Controls
-            //Debug.Log("end scene controls");
+            EndSceneControls(); //call EndSceneControls()
         }
+
     }
 
     public void StartSceneControls()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return)) //if Return/Enter is pressed...(You can change this key code!)
         {
-            //Debug.Log("Return Key Pressed");
-            SceneManager.LoadScene("MainScene");
+            SceneManager.LoadScene("MainScene"); //load the MainScene
+        }
+    }
+
+    public void MainSceneControls()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) //if Return/Enter is pressed...(You can change this key code!)
+        {
+            SceneManager.LoadScene("EndScene"); //load the EndScene
+        }
+    }
+
+    public void EndSceneControls()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) //if Return/Enter is pressed...(You can change this key code!)
+        {
+            SceneManager.LoadScene("MainScene"); //load the StartScene
         }
     }
 }
